c915396f648bd8711920413d9b984cd797068ab9
gabrielbdsm <gabrielmartiliano2@gmail.com>
2024-03-19 19:33:03 -0300

    feat: lista de tarefa syles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import {Router ,Request , Response  } from "express"
 import  * as AuthController  from "../controllers/AuthController "
+import  * as taskListController  from "../controllers/taskListController"
 const router = Router()
 
 router.get("/ping" , (req : Request , res: Response ) =>{
-    res.json({pong : true})
+res.json({pong : true})
 })
+
+router.get("/" , AuthController.showLoginForm)
 
 router.get("/cadastro" , AuthController.showRegistreForm)
 router.post("/cadastro" , AuthController.registre)
@@ -12,7 +15,13 @@ router.post("/cadastro" , AuthController.registre)
 router.get("/login" , AuthController.showLoginForm)
 router.post("/login" , AuthController.login)
 
-router.get("/agenda" , (req : Request , res: Response ) =>{
-    res.json({agenda : true})
+router.get("/Lista_tarefa" , (req : Request , res: Response ) =>{
+res.render("taskList")
 })
+
+router.post("/Lista_tarefa" ,taskListController.saveTask )
+
+router.delete("/Lista_tarefa" ,taskListController.deleteTask )
+
+
 export default router 
